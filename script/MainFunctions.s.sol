6fe313c0132d1bd571d1ae365468710d59b1963a
CarlosR <carlos.ramos@grupoimportia.com>
2023-07-25 17:21:13 -0500

    add new comment in main script'

--- script/MainFunctions.s.sol
+++ script/MainFunctions.s.sol
@@ -23,27 +23,27 @@ contract MainFunctionsScript is
     {
         startBroadcast();
         // STEP 1 : Create a subscription
-        uint64 subId = createSubscription();
-
-        // STEP 2 : Fund the subscription
-        fundSubscription(subId, 1 ether);
+        // uint64 subId = createSubscription();
 
         // NOTE : if you alredy have a funded subscription you can directly use it
-        // uint64 subId = 1897;
+        uint64 subId = 1897;
+
+        // STEP 2 : Fund the subscription
+        fundSubscription(subId, 20 ether);
 
         // STEP 3 : Deploy consumer contract
-        FunctionsConsumer consumer = new FunctionsConsumer(address(oracle()));
+        // FunctionsConsumer consumer = new FunctionsConsumer(address(oracle()));
 
         // STEP 4 : Add consumer contract to subscription
-        addConsumer(subId, address(consumer));
+        // addConsumer(subId, address(consumer));
 
         // STEP 5 : Execute a Request
-        executeRequest(
-            address(consumer),
-            subId,
-            "calculation-example.js",
-            new string[](0)
-        );
+        // executeRequest(
+        //     address(consumer),
+        //     subId,
+        //     "calculation-example.js",
+        //     new string[](0)
+        // );
 
         // [OPTIONAL] : Check Subscription Data
         printSubscriptionInfo(subId);
